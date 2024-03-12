44f71f71de960dbca377dbd4614d239fdbba0b1d
yhoungdev <yhoungdev@gmail.com>
2024-03-12 21:15:16 +0100

    update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         {/* Adding an overlay with a lower z-index to ensure content stays behind the header */}
         <div className="absolute inset-0 bg-black bg-opacity-90 z-0"></div>
         <div className="flex md:w-[800px] mx-auto flex-col justify-start h-[90vh] items-center px-4 md:px-8 pt-4 md:pt-8 relative z-10">
-          <h1 className="hero-text my-[2em] title_element text-[5em] text-center md:text-[90px] font-bold mb-4 leading-snug">
+          <h1 className="hero-text my-[1em] title_element text-[5em] text-center md:text-[90px] font-bold mb-4 leading-snug">
             MEME FINANCE LAYER OF BITCOIN
           </h1>
           <a href="https://tinyurl.com/GoatCoinSTXdiscord">
